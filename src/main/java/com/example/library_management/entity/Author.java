d722ef0141074bc44d51cec58fca958475390408
Dinhh-Chan <dinhtran29092005@gmail.com>
2024-11-03 19:52:23 +0700

    author

--- src/main/java/com/example/library_management/entity/Author.java
+++ src/main/java/com/example/library_management/entity/Author.java
@@ -1,5 +1,55 @@
 package com.example.library_management.entity;
+import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+@Entity
+@Table(name ="authors")
 public class Author {
-    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id; 
+    @Column(name = "name",nullable= false )
+    private String name; 
+    @Column(name="bio", columnDefinition= "TEXT")
+    private  String bio ;
+    @ManyToMany (mappedBy="authors")
+    private Set<Book> books;
+    public Author(){}
+    public Author(String name, String bio){
+        this.name = name;
+        this.bio =bio;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }   
 }
