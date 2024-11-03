42ad37c86a9887fbf9f3dd57727f30ccd8b53127
Dinhh-Chan <dinhtran29092005@gmail.com>
2024-11-03 20:08:12 +0700

    author repository

--- src/main/java/com/example/library_management/repository/AuthorRepository.java
+++ src/main/java/com/example/library_management/repository/AuthorRepository.java
@@ -1,5 +1,14 @@
 package com.example.library_management.repository;
 
-public class AuthorRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.library_management.entity.Author;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    // Tìm tác giả theo tên
+    Optional<Author> findByName(String name);
 }
